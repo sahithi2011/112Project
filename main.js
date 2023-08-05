5fe8eac07cda28293371189507013e8452002c28
sahithi2011 <107986565+sahithi2011@users.noreply.github.com>
2023-08-05 09:57:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:350,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'" />'
+});
+
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zjcHIvdSi/' ,modelLoaded);
